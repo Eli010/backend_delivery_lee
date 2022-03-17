d546da25804ce51b30e979bb667c7c2d81e35db5
Eli010 <87773123+Eli010@users.noreply.github.com>
2022-03-17 17:37:11 -0500

    Delete keys.js

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    //la clave es cualqueir que tu queiras, pero lo recomendable es que visites
-    //a google con la palabra secret key generatos ( radom key,ecryption)
-    secretOrKey:'UkXp2s5v8x/A?D(G+KbPeShVmYq3t6w9z$B&E)H@McQ'
-}
